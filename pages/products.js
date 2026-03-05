1ad3e44bc90cb062456269dd0a549b2fac6c31aa
neron237 <btcdollar237@gmail.com>
2026-03-05 03:11:39 +0100

    Create products.js file for products listing page

---
+++ pages/products.js
@@ -0,0 +1,13 @@
+// products.js
+
+const ProductsListing = () => {
+    return (
+        <div>
+            <h1>Products Listing</h1>
+            <p>This is the products listing page.</p>
+            {/* Add your product listing logic here */}
+        </div>
+    );
+};
+
+export default ProductsListing;

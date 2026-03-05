4ca7fdc25cb62b2f04e4143dd6b0e17bc93cb9ae
neron237 <btcdollar237@gmail.com>
2026-03-05 03:22:24 +0100

    Create Footer component at components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>&copy; 2026 KS Auto Parts. All rights reserved.</p>
+                <ul>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#privacy">Privacy Policy</a></li>
+                    <li><a href="#terms">Terms of Service</a></li>
+                </ul>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
